512b615163bff7d73983485abc451fa9fe358a7c
Antvictor <larryyangc@163.com>
2024-04-23 16:03:40 +0800

    feat: 合并文字支持自定义字体及大小

--- src/main/java/top/codejourney/ImageUtils.java
+++ src/main/java/top/codejourney/ImageUtils.java
@@ -16,21 +16,45 @@ public class ImageUtils {
 
     /**
      * 默认样式的文字合成
-     * @param initX 初始坐标 X
-     * @param initY 初始坐标 Y
-     * @param endX 截止坐标 X
-     * @param endY 截止坐标 Y
-     * @param spacing 行距
+     *
+     * @param initX       初始坐标 X
+     * @param initY       初始坐标 Y
+     * @param endX        截止坐标 X
+     * @param endY        截止坐标 Y
+     * @param spacing     行距
      * @param sourceImage 原图
-     * @param word 文字
+     * @param word        文字
      * @return
      */
     public static BufferedImage mergeWord(int initX, int initY,
                                           int endX, int endY, int spacing,
                                           BufferedImage sourceImage,
-                                          String word, String fontName
+                                          String word
     ) {
-        Font font = new Font(fontName, Font.PLAIN, 30);
+        return mergeWord(initX, initY, endX, endY, spacing, sourceImage, word, "宋体", 30);
+    }
+
+    /**
+     * 可以自定义字体的文字合成
+     *
+     * @param initX       初始坐标 X
+     * @param initY       初始坐标 Y
+     * @param endX        截止坐标 X
+     * @param endY        截止坐标 Y
+     * @param spacing     行距
+     * @param sourceImage 原图
+     * @param word        文字
+     * @param fontName    字体名称
+     * @param fontSize    字体大小
+     * @return
+     */
+    public static BufferedImage mergeWord(int initX, int initY,
+                                          int endX, int endY, int spacing,
+                                          BufferedImage sourceImage,
+                                          String word, String fontName, Integer fontSize
+    ) {
+
+        Font font = new Font(fontName, Font.PLAIN, fontSize);
         Color color = new Color(0, 0, 0);
         return mergeWord(initX, initY, endX, endY, spacing, sourceImage, word,
                 font, color, ImageAlign.LEFT, true, true, true);
@@ -38,27 +62,28 @@ public class ImageUtils {
 
     /**
      * 在指定区域绘制文字
-     * @param initX 初始坐标 X
-     * @param initY 初始坐标 Y
-     * @param endX 截止坐标 X
-     * @param endY 截止坐标 Y
-     * @param spacing 行距
-     * @param sourceImage 原图
-     * @param word 文字
-     * @param font 字体
-     * @param color 颜色
-     * @param align 文字对齐方式
-     * @param isLineFeed 是否换行
+     *
+     * @param initX             初始坐标 X
+     * @param initY             初始坐标 Y
+     * @param endX              截止坐标 X
+     * @param endY              截止坐标 Y
+     * @param spacing           行距
+     * @param sourceImage       原图
+     * @param word              文字
+     * @param font              字体
+     * @param color             颜色
+     * @param align             文字对齐方式
+     * @param isLineFeed        是否换行
      * @param isProhibitExceedX 是否禁止超出X
      * @param isProhibitExceedY 是否禁止超出Y
      * @return
      */
     public static BufferedImage mergeWord(int initX, int initY,
-                                           int endX, int endY, int spacing,
-                                           BufferedImage sourceImage,
-                                           String word,
-                                           Font font, Color color,
-                                           ImageAlign align, boolean isLineFeed,
+                                          int endX, int endY, int spacing,
+                                          BufferedImage sourceImage,
+                                          String word,
+                                          Font font, Color color,
+                                          ImageAlign align, boolean isLineFeed,
                                           boolean isProhibitExceedY,
                                           boolean isProhibitExceedX
     ) {
@@ -74,7 +99,7 @@ public class ImageUtils {
         int wordPixel = wordWidth / word.length();
         int end = (endX - initX) / wordPixel;
         if (isLineFeed) {
-            while ((graphics2D.getFontMetrics().stringWidth(word)) > 0 ) {
+            while ((graphics2D.getFontMetrics().stringWidth(word)) > 0) {
                 end = Math.min(end, word.length());
                 // 换行
                 String line = word.substring(0, end);
@@ -102,10 +127,10 @@ public class ImageUtils {
     /**
      * 合并图片到指定区域，只能在[initX, initY] [initX, endY] [endX, initY] [endX, endY] 四个点划定的区域内进行合并
      *
-     * @param initX 目标区域起始坐标 initX
-     * @param initY 目标区域起始坐标 initY
-     * @param endX 目标区域截止坐标 initX
-     * @param endY 目标区域截止坐标 initY
+     * @param initX       目标区域起始坐标 initX
+     * @param initY       目标区域起始坐标 initY
+     * @param endX        目标区域截止坐标 initX
+     * @param endY        目标区域截止坐标 initY
      * @param sourceImage 原图
      * @return
      */
@@ -114,13 +139,26 @@ public class ImageUtils {
                                            BufferedImage sourceImage,
                                            BufferedImage image,
                                            ImageAlign align
-                                           ) {
+    ) {
 
         Graphics2D graphics2D = sourceImage.createGraphics();
 
         return drawImage(initX, initY, endX, endY, sourceImage, image, align, graphics2D);
     }
 
+    /**
+     * 合并图片到指定区域，只能在[initX, initY] [initX, endY] [endX, initY] [endX, endY] 四个点划定的区域内进行合并
+     *
+     * @param initX       目标区域起始坐标 initX
+     * @param initY       目标区域起始坐标 initY
+     * @param endX        目标区域截止坐标 initX
+     * @param endY        目标区域截止坐标 initY
+     * @param sourceImage 原图
+     * @param image       合成图
+     * @param align       样式
+     * @param isScale     是否等比缩放
+     * @return
+     */
     public static BufferedImage mergeImage(int initX, int initY,
                                            int endX, int endY,
                                            BufferedImage sourceImage,
@@ -143,6 +181,19 @@ public class ImageUtils {
         return drawImage(initX, initY, endX, endY, sourceImage, image, align, graphics2D);
     }
 
+    /**
+     * 绘制图片
+     *
+     * @param initX       起始坐标 x
+     * @param initY       y
+     * @param endX        结束坐标 x
+     * @param endY        y
+     * @param sourceImage 原图
+     * @param image       合成图
+     * @param align       样式
+     * @param graphics2D  工具
+     * @return
+     */
     private static BufferedImage drawImage(int initX, int initY, int endX, int endY, BufferedImage sourceImage, BufferedImage image, ImageAlign align, Graphics2D graphics2D) {
         initX = drawXAndY(align, initX, endX, image.getWidth());
         // 横向只居中
@@ -157,16 +208,17 @@ public class ImageUtils {
 
     /**
      * 绘制坐标
-     * @param align 类型
-     * @param start 开始坐标
-     * @param end 截止坐标
+     *
+     * @param align  类型
+     * @param start  开始坐标
+     * @param end    截止坐标
      * @param length 长度
      */
     private static int drawXAndY(ImageAlign align, Integer start, Integer end, Integer length) {
         int result;
         switch (align) {
             case CENTER:
-                result = length < (end - start) ? (end - start - length) / 2 +  start : start;
+                result = length < (end - start) ? (end - start - length) / 2 + start : start;
                 break;
             case RIGHT:
                 result = length < (end - start) ? end - length : start;
